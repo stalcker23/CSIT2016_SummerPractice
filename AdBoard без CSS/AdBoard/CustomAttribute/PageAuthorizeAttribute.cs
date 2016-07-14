369da36dc0e8a0d27d8f02cd6393e09369404b18
stalcker23 <a-zenit-a@rambler.ru>
2016-07-15 02:19:52 +0300

    3.0 ZBT without good html, but without failed controller's logic, with full functional

---
+++ AdBoard без CSS/AdBoard/CustomAttribute/PageAuthorizeAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AdBoard.Models;
+namespace AdBoard.CustomAttribute
+{
+    public class PageAuthorizeAttribute : AuthorizeAttribute
+    {
+        public string UserRoles { get; set; }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var authCooke = httpContext.Request.Cookies["__AUTH"];
+
+            if (authCooke != null)
+            {
+                User user = UserModel.GetUserByCookeis(authCooke.Value);
+
+                return UserRoles.Split(',').Any(r => r.Trim().ToLower() == user.Role.RoleName.Trim().ToLower());
+            }
+
+            return false;
+        }
+    }
+}
---
+++ AdBoard без CSS/AdBoard/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using System.Linq;
+using System.Web;
+
+namespace AdBoard.ViewModels
+{
+    public class RegistrationViewModel
+    {
+        [Required(ErrorMessage = "*")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string LastName { get; set; }
+
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "Не корректный Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string DPassword { get; set; }
+    }
+}
